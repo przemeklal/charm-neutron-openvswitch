przemeklal/charm-neutron-openvswitch

commit 862c36229603bf86ffe903068ccbc12b8e27c522

ovs-dpdk: support late initialization since ovs 2.6.0
OVS 2.6.0 introduces new mechanisms to configure the DPDK netdev
provider. It now relies on the database and allows dynamic runtime
configuration. Network interface binding is more fine grained by
specifying the NIC PCI address and not relying on special port naming
and indexing.

Here we introduce the support of post 2.6.0 OVS-DPDK and change the
relevant tests.

Change-Id: Ic0185097d65df04a2b566e16cb22bcbd088eed3e

hooks/neutron_ovs_utils.py
@@ -40,6 +40,7 @@ from charmhelpers.contrib.network.ovs import (
     full_restart,
     enable_ipfix,
     disable_ipfix,
+    set_Open_vSwitch_column_value
 )
 from charmhelpers.core.hookenv import (
     config,
@@ -69,6 +70,7 @@ from charmhelpers.fetch import (
     apt_purge,
     apt_update,
     filter_installed_packages,
+    get_upstream_version
 )
 
 from pci import PCINetDevices
@@ -307,6 +309,8 @@ def resource_map():
         )
         if not use_dpdk():
             drop_config.append(DPDK_INTERFACES)
+        if ovs_has_late_dpdk_init():
+            drop_config.append(OVS_DEFAULT)
     else:
         drop_config.extend([OVS_CONF, DPDK_INTERFACES])
 
@@ -376,6 +380,17 @@ OVS_DEFAULT_BIN = '/usr/lib/openvswitch-switch/ovs-vswitchd'
 def enable_ovs_dpdk():
     '''Enables the DPDK variant of ovs-vswitchd and restarts it'''
     subprocess.check_call(UPDATE_ALTERNATIVES + [OVS_DPDK_BIN])
+    if ovs_has_late_dpdk_init():
+        ctxt = neutron_ovs_context.OVSDPDKDeviceContext()
+        set_Open_vSwitch_column_value(
+            'other_config:dpdk-init=true')
+        set_Open_vSwitch_column_value(
+            'other_config:dpdk-lcore-mask={}'.format(ctxt['cpu_mask']))
+        set_Open_vSwitch_column_value(
+            'other_config:dpdk-socket-mem={}'.format(ctxt['socket_memory']))
+        set_Open_vSwitch_column_value(
+            'other_config:dpdk-extra=--vhost-owner'
+            ' libvirt-qemu:kvm --vhost-perm 0660')
     if not is_unit_paused_set():
         service_restart('openvswitch-switch')
 
@@ -411,12 +426,12 @@ def configure_ovs():
     else:
         # NOTE: when in dpdk mode, add based on pci bus order
         #       with type 'dpdk'
-        dpdk_bridgemaps = neutron_ovs_context.resolve_dpdk_ports()
+        bridgemaps = neutron_ovs_context.resolve_dpdk_ports()
         device_index = 0
-        for br in dpdk_bridgemaps.values():
+        for pci_address, br in bridgemaps.items():
             add_bridge(br, datapath_type)
             dpdk_add_bridge_port(br, 'dpdk{}'.format(device_index),
-                                 port_type='dpdk')
+                                 pci_address)
             device_index += 1
 
     target = config('ipfix-target')
@@ -560,6 +575,13 @@ def use_dpdk():
     return (cmp_release >= 'mitaka' and config('enable-dpdk'))
 
 
+def ovs_has_late_dpdk_init():
+    ''' OVS 2.6.0 introduces late initialization '''
+    import apt_pkg
+    ovs_version = get_upstream_version("openvswitch-switch")
+    return apt_pkg.version_compare(ovs_version, '2.6.0') >= 0
+
+
 def enable_sriov():
     '''Determine whether SR-IOV is enabled and supported'''
     cmp_release = CompareOpenStackReleases(
@@ -568,13 +590,19 @@ def enable_sriov():
 
 
 # TODO: update into charm-helpers to add port_type parameter
-def dpdk_add_bridge_port(name, port, promisc=False, port_type=None):
+def dpdk_add_bridge_port(name, port, pci_address=None):
     ''' Add a port to the named openvswitch bridge '''
     # log('Adding port {} to bridge {}'.format(port, name))
-    cmd = ["ovs-vsctl", "--", "--may-exist", "add-port", name, port]
-    if port_type:
-        cmd += ['--', 'set', 'Interface', port,
-                'type={}'.format(port_type)]
+    if ovs_has_late_dpdk_init():
+        cmd = ["ovs-vsctl",
+               "add-port", name, port, "--",
+               "set", "Interface", port,
+               "type=dpdk",
+               "options:dpdk-devargs={}".format(pci_address)]
+    else:
+        cmd = ["ovs-vsctl", "--",
+               "--may-exist", "add-port", name, port,
+               "--", "set", "Interface", port, "type=dpdk"]
     subprocess.check_call(cmd)
 
 

unit_tests/test_neutron_ovs_utils.py
@@ -55,6 +55,7 @@ TO_PATCH = [
     'PCINetDevices',
     'enable_ipfix',
     'disable_ipfix',
+    'ovs_has_late_dpdk_init',
 ]
 
 head_pkg = 'linux-headers-3.15.0-5-generic'
@@ -92,6 +93,7 @@ class TestNeutronOVSUtils(CharmTestCase):
         self.neutron_plugin_attribute.side_effect = _mock_npa
         self.config.side_effect = self.test_config.get
         self.use_dpdk.return_value = False
+        self.ovs_has_late_dpdk_init.return_value = False
 
     def tearDown(self):
         # Reset cached cache
@@ -470,11 +472,8 @@ class TestNeutronOVSUtils(CharmTestCase):
         ])
         self.add_bridge_port.assert_called_with('br-ex', 'eth0')
 
-    @patch.object(neutron_ovs_context, 'resolve_dpdk_ports')
-    @patch.object(nutils, 'use_dvr')
-    @patch('charmhelpers.contrib.openstack.context.config')
-    def test_configure_ovs_dpdk(self, mock_config, _use_dvr,
-                                _resolve_dpdk_ports):
+    def _run_configure_ovs_dpdk(self, mock_config, _use_dvr,
+                                _resolve_dpdk_ports, _late_init):
         _resolve_dpdk_ports.return_value = OrderedDict([
             ('0000:001c.01', 'br-phynet1'),
             ('0000:001c.02', 'br-phynet2'),
@@ -482,6 +481,7 @@ class TestNeutronOVSUtils(CharmTestCase):
         ])
         _use_dvr.return_value = True
         self.use_dpdk.return_value = True
+        self.ovs_has_late_dpdk_init.return_value = _late_init
         mock_config.side_effect = self.test_config.get
         self.config.side_effect = self.test_config.get
         self.test_config.set('enable-dpdk', True)
@@ -495,11 +495,27 @@ class TestNeutronOVSUtils(CharmTestCase):
             any_order=True
         )
         self.dpdk_add_bridge_port.assert_has_calls([
-            call('br-phynet1', 'dpdk0', port_type='dpdk'),
-            call('br-phynet2', 'dpdk1', port_type='dpdk'),
-            call('br-phynet3', 'dpdk2', port_type='dpdk')],
+            call('br-phynet1', 'dpdk0', '0000:001c.01'),
+            call('br-phynet2', 'dpdk1', '0000:001c.02'),
+            call('br-phynet3', 'dpdk2', '0000:001c.03')],
             any_order=True
         )
+
+    @patch.object(neutron_ovs_context, 'resolve_dpdk_ports')
+    @patch.object(nutils, 'use_dvr')
+    @patch('charmhelpers.contrib.openstack.context.config')
+    def test_configure_ovs_dpdk(self, mock_config, _use_dvr,
+                                _resolve_dpdk_ports):
+        return self._run_configure_ovs_dpdk(mock_config, _use_dvr,
+                                            _resolve_dpdk_ports, False)
+
+    @patch.object(neutron_ovs_context, 'resolve_dpdk_ports')
+    @patch.object(nutils, 'use_dvr')
+    @patch('charmhelpers.contrib.openstack.context.config')
+    def test_configure_ovs_dpdk_late_init(self, mock_config, _use_dvr,
+                                          _resolve_dpdk_ports):
+        return self._run_configure_ovs_dpdk(mock_config, _use_dvr,
+                                            _resolve_dpdk_ports, True)
 
     @patch.object(nutils, 'use_dvr')
     @patch('charmhelpers.contrib.openstack.context.config')

commit e8af22633b2a3e8babc6163e1fd2609f8eb736b1

Fix charm upgrade broken by commit 862c362
Currently, upgrading this charm on a host that is running
ovs >= 2.6 will break because the OVS_DEFAULT config file
is not expected to be written by the charm.

Change-Id: I33352deb3b60231347045d5f39f3508a29dda61e

hooks/neutron_ovs_hooks.py
@@ -62,21 +62,25 @@ def install():
 # for the implications of modifications to the /etc/default/openvswitch-switch.
 @hooks.hook('upgrade-charm')
 def upgrade_charm():
-    # In the 16.10 release of the charms, the code changed from managing the
-    # /etc/default/openvswitch-switch file only when dpdk was enabled to always
-    # managing this file. Thus, an upgrade of the charm from a release prior
-    # to 16.10 or higher will always cause the contents of the file to change
-    # and will trigger a restart of the openvswitch-switch service, which in
-    # turn causes a temporary network outage. To prevent this outage, determine
-    # if the /etc/default/openvswitch-switch file needs to be migrated and if
-    # so, migrate the file but do NOT restart the openvswitch-switch service.
-    # See bug LP #1712444
-    with open(OVS_DEFAULT, 'r') as f:
-        # The 'Service restart triggered ...' line was added to the OVS_DEFAULT
-        # template in the 16.10 version of the charm to allow restarts so we
-        # use this as the key to see if the file needs migrating.
-        if 'Service restart triggered' not in f.read():
-            CONFIGS.write(OVS_DEFAULT)
+    if OVS_DEFAULT in restart_map():
+        # In the 16.10 release of the charms, the code changed from managing
+        # the /etc/default/openvswitch-switch file only when dpdk was enabled
+        # to always managing this file. Thus, an upgrade of the charm from a
+        # release prior to 16.10 or higher will always cause the contents of
+        # the file to change and will trigger a restart of the
+        # openvswitch-switch service, which in turn causes a temporary
+        # network outage. To prevent this outage, determine if the
+        # /etc/default/openvswitch-switch file needs to be migrated and if
+        # so, migrate the file but do NOT restart the openvswitch-switch
+        # service.
+        # See bug LP #1712444
+        with open(OVS_DEFAULT, 'r') as f:
+            # The 'Service restart triggered ...' line was added to the
+            # OVS_DEFAULT template in the 16.10 version of the charm to allow
+            # restarts so we use this as the key to see if the file needs
+            # migrating.
+            if 'Service restart triggered' not in f.read():
+                CONFIGS.write(OVS_DEFAULT)
 
 
 @hooks.hook('neutron-plugin-relation-changed')

hooks/neutron_ovs_utils.py
@@ -309,7 +309,8 @@ def resource_map():
         )
         if not use_dpdk():
             drop_config.append(DPDK_INTERFACES)
-        if ovs_has_late_dpdk_init():
+            drop_config.append(OVS_DEFAULT)
+        elif ovs_has_late_dpdk_init():
             drop_config.append(OVS_DEFAULT)
     else:
         drop_config.extend([OVS_CONF, DPDK_INTERFACES])

unit_tests/test_neutron_ovs_hooks.py
@@ -67,8 +67,9 @@ class NeutronOVSHooksTests(CharmTestCase):
         self._call_hook('install')
         self.install_packages.assert_called_with()
 
+    @patch.object(hooks, 'restart_map')
     @patch.object(hooks, 'restart_on_change')
-    def test_migrate_ovs_default_file(self, mock_restart):
+    def test_migrate_ovs_default_file(self, mock_restart, mock_restart_map):
         # Tests that the /etc/default/openvswitch-switch file is/isn't
         # migrated on the upgrade-charm hook and that no restarts are
         # attempted of the openvswitch-switch service.
@@ -82,14 +83,22 @@ class NeutronOVSHooksTests(CharmTestCase):
             with open('unit_tests/%s' % sample, 'r') as f:
                 content = f.read()
 
-            with patch('builtins.open', mock_open(read_data=content),
-                       create=True):
-                self._call_hook('upgrade-charm')
-                if should_migrate:
-                    self.CONFIGS.write.assert_called_with(utils.OVS_DEFAULT)
+            for ovs_default_exists in [False, True]:
+                if ovs_default_exists:
+                    mock_restart_map.return_value = {utils.OVS_DEFAULT: {}}
                 else:
-                    self.CONFIGS.write.assert_not_called()
-                self.assertEqual(0, mock_restart.call_count)
+                    mock_restart_map.return_value = {}
+
+                with patch('builtins.open', mock_open(read_data=content),
+                           create=True):
+                    self.CONFIGS.write.reset_mock()
+                    self._call_hook('upgrade-charm')
+                    if should_migrate and ovs_default_exists:
+                        self.CONFIGS.write.assert_called_with(
+                            utils.OVS_DEFAULT)
+                    else:
+                        self.CONFIGS.write.assert_not_called()
+                    self.assertEqual(0, mock_restart.call_count)
 
     def test_config_changed_dvr(self):
         self._call_hook('config-changed')

unit_tests/test_neutron_ovs_utils.py
@@ -256,7 +256,6 @@ class TestNeutronOVSUtils(CharmTestCase):
         _regconfs = nutils.register_configs()
         confs = ['/etc/neutron/neutron.conf',
                  '/etc/neutron/plugins/ml2/openvswitch_agent.ini',
-                 '/etc/default/openvswitch-switch',
                  '/etc/init/os-charm-phy-nic-mtu.conf']
         self.assertEqual(_regconfs.configs, confs)
 
@@ -331,11 +330,11 @@ class TestNeutronOVSUtils(CharmTestCase):
     def test_resource_map_dhcp(self, _use_dvr, _enable_local_dhcp):
         _enable_local_dhcp.return_value = True
         _use_dvr.return_value = False
-        self.os_release.return_value = 'diablo'
-        self.lsb_release.return_value = {'DISTRIB_CODENAME': 'lucid'}
+        self.os_release.return_value = 'mitaka'
+        self.lsb_release.return_value = {'DISTRIB_CODENAME': 'xenial'}
         _map = nutils.resource_map()
-        svcs = ['neutron-plugin-openvswitch-agent', 'neutron-metadata-agent',
-                'neutron-dhcp-agent']
+        svcs = ['neutron-metadata-agent', 'neutron-dhcp-agent',
+                'neutron-openvswitch-agent']
         confs = [nutils.NEUTRON_CONF, nutils.NEUTRON_METADATA_AGENT_CONF,
                  nutils.NEUTRON_DHCP_AGENT_CONF]
         [self.assertIn(q_conf, _map.keys()) for q_conf in confs]
@@ -367,20 +366,20 @@ class TestNeutronOVSUtils(CharmTestCase):
         _map = nutils.resource_map()
         self.assertFalse(nutils.EXT_PORT_CONF in _map.keys())
 
+    @patch.object(nutils, 'use_dpdk')
     @patch.object(nutils, 'use_dvr')
-    def test_restart_map(self, _use_dvr):
-        _use_dvr.return_value = False
-        self.os_release.return_value = "diablo"
-        self.lsb_release.return_value = {'DISTRIB_CODENAME': 'lucid'}
+    def test_restart_map(self, mock_use_dvr, mock_use_dpdk):
+        mock_use_dvr.return_value = False
+        mock_use_dpdk.return_value = False
+        self.os_release.return_value = "mitaka"
+        self.lsb_release.return_value = {'DISTRIB_CODENAME': 'xenial'}
+        ML2CONF = "/etc/neutron/plugins/ml2/openvswitch_agent.ini"
         _restart_map = nutils.restart_map()
-        ML2CONF = "/etc/neutron/plugins/ml2/ml2_conf.ini"
         expect = OrderedDict([
-            (nutils.NEUTRON_CONF, ['neutron-plugin-openvswitch-agent']),
-            (ML2CONF, ['neutron-plugin-openvswitch-agent']),
-            (nutils.OVS_DEFAULT, ['openvswitch-switch']),
-            (nutils.PHY_NIC_MTU_CONF, ['os-charm-phy-nic-mtu'])
+            (ML2CONF, ['neutron-openvswitch-agent']),
+            (nutils.NEUTRON_CONF, ['neutron-openvswitch-agent']),
         ])
-        self.assertEqual(expect, _restart_map)
+        self.assertEqual(expect, OrderedDict(_restart_map))
         for item in _restart_map:
             self.assertTrue(item in _restart_map)
             self.assertTrue(expect[item] == _restart_map[item])